046865e97e96bfe0c669971fd5df8ea2ef9b073f
EC2 Default User <ec2-user@ip-172-31-31-68.us-west-2.compute.internal>
2022-05-08 07:41:13 +0000

    finishu

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,6 +60,7 @@
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
+                                    @csrf
                                     {{ __('Login') }}
                                 </button>
 
